e874f2df3869bc3beaf3cf2693302f47c91f9dc4
Danial-Waheed <daniyalwahied@gmail.com>
2023-09-28 13:47:29 +0500

    modules % require

---
+++ modules.js
@@ -0,0 +1,7 @@
+const { persons, ranks } = require("./persons");
+
+console.log(persons, ranks);
+
+const os = require("os");
+
+console.log(os.platform(), os.homedir());
---
+++ persons.js
@@ -0,0 +1,7 @@
+const persons = ["Danial", "Hanzila", "Usman", "Hamza"];
+const ranks = ["1st", "2nd", "3rd", "4th"];
+
+module.exports = {
+  persons,
+  ranks,
+};
