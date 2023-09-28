74ecc91d0c8d593a639093f7e69f3e8afddb95d3
Danial-Waheed <daniyalwahied@gmail.com>
2023-09-28 14:40:28 +0500

    returning HTML

--- Clients & Servers/server.js
+++ Clients & Servers/server.js
@@ -1,7 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log("request made");
+
+  //   set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  // send an html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
