dbcca96a67c1361ad38d9dce53e5c0cf71d6a439
Danial-Waheed <daniyalwahied@gmail.com>
2023-09-28 14:47:30 +0500

    basic routing, redirects & status codes

--- Clients & Servers/server.js
+++ Clients & Servers/server.js
@@ -7,8 +7,29 @@ const server = http.createServer((req, res) => {
   //   set header content type
   res.setHeader("Content-Type", "text/html");
 
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
   // send an html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
@@ -21,3 +42,16 @@ const server = http.createServer((req, res) => {
 server.listen(3000, "localhost", () => {
   console.log("listening for requests on port 3000");
 });
+
+// Status Codes
+
+// 200 - OK
+// 301 - Resource moved
+// 404 - Not found
+// 500 - Internal server error
+
+// 100 Range - Informational response - request received, continuing process
+// 200 Range - Success - The action was successfully received, understood, and accepted
+// 300 Range - Redirection - Further action must be taken in order to complete the request
+// 400 Range - Client Error - The request contains bad syntax or cannot be fulfilled
+// 500 Range - Server Error - The server failed to fulfill an apparently valid request
